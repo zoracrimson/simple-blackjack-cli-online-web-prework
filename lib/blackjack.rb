b45dd6afce60e9de4a3d2adbb75e435ec1a9ec70
Kat Maldon <katharinamaldon@gmail.com>
2018-12-04 17:02:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,29 +1,33 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11].sample
 end
 
 def display_card_total
-  # code #display_card_total here
+  card_total = deal_card + deal_card
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  2.times do deal_card 
+    return display_card_total
+    puts display_card_total
+  end
 end
 
 def hit?
